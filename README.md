6f9f5ca8e15203b7d8d5bf6a495f6f6f2bb41ace
David McCaslin <davidmccaslin94@gmail.com>
2020-08-24 11:46:14 -0700

    update readme

---
+++ README.md
@@ -0,0 +1,34 @@
+# Address Book
+
+#### Tool for creating, updating, and viewing contacts
+
+#### By David McCaslin
+
+## Description
+
+This program allows users to add, delete, and lookup contacts. Contacts are stored as an object and may contain various fields of information.
+
+## Setup/Installation Requirements
+
+* Find repository on GitHub
+* Fork/clone/download project
+* Open project in IDE and/or view through a browswer
+
+## Known Bugs
+
+There are no known bugs.
+
+## Support and contact details
+
+Contact: davidmccaslin94@gmail.com
+
+## Technologies Used
+
+* JavaScript
+* VSCode, Git, GitHub
+
+### License
+
+MIT Open Source
+
+Copyright (c) 2020 David McCaslin
--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,30 @@ AddressBook.prototype.assignId = function() {
   return this.currentId;
 }
 
+AddressBook.prototype.findContact = function(id) {
+  for (let i = 0; i < this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+      }
+    }
+  };
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  for (let i = 0; i < this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        delete this.contacts[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
+
+
 // Business logic for Contacts
 
 function Contact(firstName, lastName, phoneNumber) {
